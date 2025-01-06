2c5a0470e0f870adab7065f4d7bef00b44234777
PrudenceGikundi <prudencekendi101@gmail.com>
2025-01-06 23:17:02 +0300

    task2addition

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js Project!</h1>
+      <p>This is the home page of my new Next.js application.</p>
+    </div>
+  );
+};
+
+export default Home;
