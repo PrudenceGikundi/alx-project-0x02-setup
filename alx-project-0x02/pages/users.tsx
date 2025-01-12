bf1268f9ac313409680479b8e62a02bc97f7d54b
PrudenceGikundi <prudencekendi101@gmail.com>
2025-01-12 09:59:10 +0300

    task

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,9 +1,8 @@
 // pages/users.tsx
 import React from 'react';
 import { GetStaticProps } from 'next';
-import UserCard from '@/components/common/UserCard'; 
+import UserCard from '@/components/common/UserCard';  
 import { UserProps } from '@/interfaces'; 
-import Header from '@/components/layout/Header';
 
 interface UsersPageProps {
   users: UserProps[];
@@ -12,7 +11,6 @@ interface UsersPageProps {
 const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div className="max-w-4xl mx-auto p-6">
-      <Header/>
       <h1 className="text-3xl font-bold text-gray-900 mb-6">Users</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {users.map((user) => (
@@ -28,7 +26,7 @@ const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
-// Correctly implement getStaticProps to fetch users data
+// getStaticProps to fetch data at build time
 export const getStaticProps: GetStaticProps = async () => {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
@@ -47,14 +45,14 @@ export const getStaticProps: GetStaticProps = async () => {
 
     return {
       props: {
-        users, // Passing users data to the component
+        users,
       },
     };
   } catch (error) {
     console.error('Error fetching users:', error);
     return {
       props: {
-        users: [], // Return an empty array if there's an error
+        users: [], // In case of error, return an empty array
       },
     };
   }
