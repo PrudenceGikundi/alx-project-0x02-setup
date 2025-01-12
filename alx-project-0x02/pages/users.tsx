7ca76545258c322669776f89c766265c129bbe92
PrudenceGikundi <prudencekendi101@gmail.com>
2025-01-12 10:10:14 +0300

    task8

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,8 +1,9 @@
 // pages/users.tsx
 import React from 'react';
 import { GetStaticProps } from 'next';
-import UserCard from '@/components/common/UserCard';  
-import { UserProps } from '@/interfaces'; 
+import UserCard from '@/components/common/UserCard';
+import { UserProps } from '@/interfaces';
+import Header from '@/components/layout/Header';
 
 interface UsersPageProps {
   users: UserProps[];
@@ -11,10 +12,12 @@ interface UsersPageProps {
 const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div className="max-w-4xl mx-auto p-6">
+      <Header />
       <h1 className="text-3xl font-bold text-gray-900 mb-6">Users</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {users.map((user) => (
           <UserCard
+            key={user.id}
             id={user.id}
             name={user.name}
             email={user.email}
